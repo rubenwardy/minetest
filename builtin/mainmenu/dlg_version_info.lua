e477762976d46595d43e920199b47c138c618240
SmallJoker <mk939@ymail.com>
2020-07-10 19:09:14 +0200

    Implement properly

--- builtin/mainmenu/dlg_version_info.lua
+++ builtin/mainmenu/dlg_version_info.lua
@@ -17,23 +17,23 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ]]
 
-local minetest_url = "https://www.minetest.net/downloads/"
+local minetest_url -- Filled by HTTP callback
 
 local function version_info_formspec(data)
 	return (
 		"formspec_version[3]" ..
-		"size[9,4.5,true]" ..
-		"textarea[0.5,0.5;8.5,3;;" ..
-		fgettext("New Minetest version is available") .. ";" ..
+		"size[10,5.5,true]" ..
+		"textarea[0.5,0.5;9.5,3;;" ..
+		core.colorize("#0E0", fgettext("A new Minetest version is available")) .. ";" ..
 		fgettext("Current version: $1\nNew version: $2\n\n" ..
 			"Visit $3 to find out how to get the newest version to stay up to date" ..
 			" with the features and bugfixes.",
 			core.get_version().string, data.new_version,
 			minetest_url) .. "]" ..
-		"button[1.5,4;4,0.75;version_check_never;" ..
-			fgettext("Disable all update notifications") .. "]" ..
-		"button[1,3;3,0.75;version_check_visit;" .. fgettext("Visit website") .. "]" ..
-		"button[5,3;3,0.75;version_check_remind;" .. fgettext("Remind me later") .. "]"
+		"button[1.5,3.5;3,0.75;version_check_visit;" .. fgettext("Visit website") .. "]" ..
+		"button[5.5,3.5;3,0.75;version_check_remind;" .. fgettext("Remind me later") .. "]" ..
+		"button[2.5,4.5;5,0.75;version_check_never;" ..
+			fgettext("Disable notifications") .. "]"
 	)
 end
 
@@ -87,38 +87,40 @@ function check_new_version()
 	end
 	core.settings:set("update_last_checked", tostring(time_now))
 
-	local http = minetest.get_http_api()
+	local http = core.get_http_api()
 	local result = http.fetch_sync({url=url})
 
 	local json = result.succeeded and core.parse_json(result.data)
-	if type(json) ~= "table" then
-		core.log("error", "Failed to read JSON output from " .. url ...
-			", status code = " .. result.code))
+	if type(json) ~= "table" or not json.latest then
+		core.log("error", "Failed to read JSON output from " .. url ..
+			", status code = " .. result.code)
 		return
 	end
 
 	local known_update = tonumber(core.settings:get("update_last_known")) or 0
-	local newest_version = json.latest.version
-	local installed_version = core.get_version().string
+	-- Format: MMNNPPP (Major, Minor, Patch)
+	local new_number = json.latest.version_code
+	minetest_url = json.latest.url
 
-	-- Expect the format Major.Minor.Patch for both
-	local nv_major, nv_minor, nv_patch = newest_version:match("^(%d+).(%d+).(%d+)")
-	local iv_major, iv_minor, iv_patch = installed_version:match("^(%d+).(%d+).(%d+)")
-	if not nv_patch or not iv_patch then
+	-- Format: Major.Minor.Patch
+	-- Convert to MMMNNNPPP
+	local cur_string = core.get_version().string
+	local cur_major, cur_minor, cur_patch = cur_string:match("^(%d+).(%d+).(%d+)")
+
+	if not new_number or not cur_patch then
 		core.log("error", "Failed to read version numbers (invalid tag format?)")
 		return
 	end
 
-	local nv_number = nv_major * 0x10000 + nv_minor * 0x100 + nv_patch
-	local iv_number = iv_major * 0x10000 + iv_minor * 0x100 + iv_patch
-	if nv_number <= known_update or nv_number < iv_number then
+	local cur_number = (cur_major * 1000 + cur_minor) * 1000 + cur_patch
+	if new_number <= known_update or new_number < cur_number then
 		return
 	end
 	-- Also consider updating from 1.2.3-dev to 1.2.3
-	if nv_number == iv_number and not core.get_version().is_dev then
+	if new_number == cur_number and not core.get_version().is_dev then
 		return
 	end
 
-	core.settings:set("update_last_known", tostring(nv_number))
-	return newest_version
+	core.settings:set("update_last_known", tostring(new_number))
+	return json.latest.version
 end
